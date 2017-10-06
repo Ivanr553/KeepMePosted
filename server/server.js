1cbc169baf1c31100914a335dfdd0025bd768286
Ivan Raffucci <ivanr553@gmail.com>
2017-10-06 12:38:55 -0700

    added more dev dependencies and connected db

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@
 let express = require('express')
 let fs = require('fs')
 let path = require('path')
+let mongoose = require('mongoose')
 
 //Initializing app
 let app = express()
 
 //Imports
-var indexRoute = require('./routes/index')
+let indexRoute = require('./routes/index')
+let db = require('../config/database')
 
 //View Engine
 app.set('view engine', 'html')
@@ -24,6 +26,13 @@ app.use('/', indexRoute)
 //Error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500)
+})
+
+//Connect to database
+mongoose.connect(db.db)
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database')
 })
 
 module.exports = app
